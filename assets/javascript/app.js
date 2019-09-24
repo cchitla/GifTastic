0da90cbedf295f26fd3e8e048c8d6230354e9d99
Chris Chitla <chrischitla@gmail.com>
2019-09-24 14:58:00 -0700

    added data state and still and moving urls for each returned gif, added on click function for each gif to play and stop (switch src urls)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,11 @@
-//activities Sep 17
-    //log movie name
-    // click JSON
-
 $(document).ready(function () {
     let avatarChars = ["Aang", "Katara", "Sokka", "Zuko", "Iroh", "Toph"];
 
-    
-    function displayButtons () {
+
+    function displayButtons() {
         $("#display-buttons").empty();
 
-        for (let i = 0; i <avatarChars.length; i++) {
+        for (let i = 0; i < avatarChars.length; i++) {
             let newBtn = $("<button>");
             newBtn.addClass("character");
             newBtn.attr("data-char-name", avatarChars[i]);
@@ -20,49 +16,65 @@ $(document).ready(function () {
     }
 
     displayButtons();
-    
 
-    $("#add-char").on("click", function(event) {
+
+    $("#add-char").on("click", function (event) {
         event.preventDefault();
 
         let newChar = $("#new-char-input").val();
         avatarChars.push(newChar);
-        
+
         displayButtons();
-        
-    })
+    });
 
 
-    $(".character").on("click", function(event){
+    $(".character").on("click", function (event) {
         let charName = $(this).attr("data-char-name");
 
         const apiKey = "yGhQanruWc9yqqX5SK0T46I5F1oxABkW";
-        const queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${charName}&limit=10`
-    
-    
+        const queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${charName}&limit=10`;
+
         $.ajax({
             url: queryURL,
             method: "GET",
-        }).then(function(response){
+        }).then(function (response) {
             $("#display-gifs").empty();
 
             let gifs = response.data;
-            
-            for (let i = 0; i < gifs.length; i++) {
-                let newGif = $("<img>")
-                newGif.attr("src", gifs[i].images.fixed_width.url);
-                $("#display-gifs").append(newGif);
-            }
+            console.log(gifs);
 
-        }, function(error){
+
+            for (let i = 0; i < gifs.length; i++) {
+                let newGif = $("<img>");
+                newGif.addClass("returned-gif");
+
+                newGif.attr("data-still", gifs[i].images.fixed_width_still.url);
+                newGif.attr("data-play", gifs[i].images.fixed_width.url);
+                newGif.attr("data-state", "still");
+                
+                newGif.attr("src", gifs[i].images.fixed_width_still.url);
+                $("#display-gifs").append(newGif);
+            };
+
+        }, function (error) {
             console.error(error);
-            
+
         });
     })
-    
 
 
+    $("#display-gifs").on("click", ".returned-gif", function () {
+        let play = $(this).attr("data-play");
+        let stop = $(this).attr("data-still");
 
+        if ($(this).attr("data-state") === "still") {
+            $(this).attr("src", play);
+            $(this).attr("data-state", "play");
+        } else {
+            $(this).attr("src", stop);
+            $(this).attr("data-state", "still");
+        };
 
+    });
 
 })
