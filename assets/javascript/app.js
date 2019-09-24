a6cf2f657f2a2ace4519ada91b4f4c7cb1f7c0a2
Chris Chitla <chrischitla@gmail.com>
2019-09-24 12:44:59 -0700

    added basic files and setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+//activities Sep 17
+    //log movie name
+    // click JSON
+
+$(document).ready(function(){
+    let avatarChars = ["Aang", "Katara", "Sokka", "Zuko", "Iroh", "Toph"];
+
+    console.log("test");
+    
+
+
+
+
+
+
+
+})
